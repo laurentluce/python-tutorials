b5c2c2cf5da00162526a3f4aa7ba3c0bb5d633ea
Laurent Luce <laurentluce49@yahoo.com>
2012-07-24 19:22:48 -0700

    fix indentation

--- threads/lock.py
+++ threads/lock.py
@@ -3,56 +3,56 @@ import urllib2
 import threading
 
 class FetchUrls(threading.Thread):
-  """
-  Thread checking URLs.
-  """
+    """
+    Thread checking URLs.
+    """
 
-  def __init__(self, urls, output, lock):
-    """
-    Constructor.
+    def __init__(self, urls, output, lock):
+        """
+        Constructor.
 
-    @param urls list of urls to check
-    @param output file to write urls output
-    """
-    threading.Thread.__init__(self)
-    self.urls = urls
-    self.output = output
-    self.lock = lock
-  
-  def run(self):
-    """
-    Thread run method. Check URLs one by one.
-    """
-    while self.urls:
-      url = self.urls.pop()
-      req = urllib2.Request(url)
-      try:
-        d = urllib2.urlopen(req)
-      except urllib2.URLError, e:
-        print 'URL %s failed: %s' % (url, e.reason)
-      self.lock.acquire()
-      print 'lock acquired by %s' % self.name
-      self.output.write(d.read())
-      print 'write done by %s' % self.name
-      print 'lock released by %s' % self.name
-      self.lock.release()
-      print 'URL %s fetched by %s' % (url, self.name)
+        @param urls list of urls to check
+        @param output file to write urls output
+        """
+        threading.Thread.__init__(self)
+        self.urls = urls
+        self.output = output
+        self.lock = lock
+    
+    def run(self):
+        """
+        Thread run method. Check URLs one by one.
+        """
+        while self.urls:
+            url = self.urls.pop()
+            req = urllib2.Request(url)
+            try:
+                d = urllib2.urlopen(req)
+            except urllib2.URLError, e:
+                print 'URL %s failed: %s' % (url, e.reason)
+            self.lock.acquire()
+            print 'lock acquired by %s' % self.name
+            self.output.write(d.read())
+            print 'write done by %s' % self.name
+            print 'lock released by %s' % self.name
+            self.lock.release()
+            print 'URL %s fetched by %s' % (url, self.name)
 
 def main():
-  # list 1 of urls to fetch
-  urls1 = ['http://www.google.com', 'http://www.facebook.com']
-  # list 2 of urls to fetch
-  urls2 = ['http://www.yahoo.com', 'http://www.youtube.com']
-  lock = threading.Lock()
-  f = open('output.txt', 'w+')
-  t1 = FetchUrls(urls1, f, lock)
-  t2 = FetchUrls(urls2, f, lock)
-  t1.start()
-  t2.start()
-  t1.join()
-  t2.join()
-  f.close()
+    # list 1 of urls to fetch
+    urls1 = ['http://www.google.com', 'http://www.facebook.com']
+    # list 2 of urls to fetch
+    urls2 = ['http://www.yahoo.com', 'http://www.youtube.com']
+    lock = threading.Lock()
+    f = open('output.txt', 'w+')
+    t1 = FetchUrls(urls1, f, lock)
+    t2 = FetchUrls(urls2, f, lock)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    f.close()
 
 if __name__ == '__main__':
-  main()
+    main()
  
--- threads/queue.py
+++ threads/queue.py
@@ -4,64 +4,65 @@ import random
 import Queue
 
 class Producer(threading.Thread):
-  """
-  Produces random integers to a list
-  """
+    """
+    Produces random integers to a list
+    """
 
-  def __init__(self, queue):
-    """
-    Constructor.
+    def __init__(self, queue):
+        """
+        Constructor.
 
-    @param integers list of integers
-    @param queue queue synchronization object
-    """
-    threading.Thread.__init__(self)
-    self.queue = queue
-  
-  def run(self):
-    """
-    Thread run method. Append random integers to the integers list at random time.
-    """
-    while True:
-      integer = random.randint(0, 256)
-      self.queue.put(integer) 
-      print '%d put to queue by %s' % (integer, self.name)
-      time.sleep(1)
+        @param integers list of integers
+        @param queue queue synchronization object
+        """
+        threading.Thread.__init__(self)
+        self.queue = queue
+    
+    def run(self):
+        """
+        Thread run method. Append random integers to the integers
+        list at random time.
+        """
+        while True:
+            integer = random.randint(0, 256)
+            self.queue.put(integer) 
+            print '%d put to queue by %s' % (integer, self.name)
+            time.sleep(1)
 
 class Consumer(threading.Thread):
-  """
-  Consumes random integers from a list
-  """
+    """
+    Consumes random integers from a list
+    """
 
-  def __init__(self, queue):
-    """
-    Constructor.
+    def __init__(self, queue):
+        """
+        Constructor.
 
-    @param integers list of integers
-    @param queue queue synchronization object
-    """
-    threading.Thread.__init__(self)
-    self.queue = queue
-  
-  def run(self):
-    """
-    Thread run method. Consumes integers from list
-    """
-    while True:
-      integer = self.queue.get()
-      print '%d popped from list by %s' % (integer, self.name)
-      self.queue.task_done()
+        @param integers list of integers
+        @param queue queue synchronization object
+        """
+        threading.Thread.__init__(self)
+        self.queue = queue
+    
+    def run(self):
+        """
+        Thread run method. Consumes integers from list
+        """
+        while True:
+            integer = self.queue.get()
+            print '%d popped from list by %s' % (integer, self.name)
+            self.queue.task_done()
 
 def main():
-  integers = []
-  queue = Queue.Queue()
-  t1 = Producer(queue)
-  t2 = Consumer(queue)
-  t1.start()
-  t2.start()
-  t1.join()
-  t2.join()
+    integers = []
+    queue = Queue.Queue()
+    t1 = Producer(queue)
+    t2 = Consumer(queue)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 
 if __name__ == '__main__':
-  main()
+    main()
  
